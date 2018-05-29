8123ecada0c0261d6665cf8f80bf59a8e762f94d
Bharat Raj Mulchandani <35489961+bharat-raj@users.noreply.github.com>
2018-05-29 09:04:53 +0530

    Add files via upload

---
+++ stack/rev.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+class Node
+{	
+	char data;
+	Node next;
+	Node(char num)
+	{
+		data=num;
+		next=null;
+	}
+	public char top()
+	{
+		Node start=this;
+		return start.data;
+	}
+
+	public int stacksize()
+	{
+		int num=0;
+		Node start=this;
+		while(start!=null)
+		{
+			start=start.next;
+			num++;
+		}
+		return num;
+	}
+	public Node push(char num)
+	{
+		Node start=this;
+		Node newnode=new Node(num);
+		newnode.next=start;
+		return newnode;
+	}
+	public Node pop()
+	{
+		Node start=this;
+		if(start==null)
+			return null;
+		return start.next;
+	}
+	public void printlist()
+	{
+		Node start=this;
+		log("list is");
+		while(start!=null)
+		{
+			log(start.data);
+			start=start.next;
+		}
+	}
+	public void log(char num)
+	{
+		System.out.println(num);	
+	}
+	public void log(int num)
+	{
+		System.out.println(num);
+	}
+	public void log(String string)
+	{
+		System.out.println(string);
+	}
+
+}
+public class rev
+{
+	public static void main(String[] args) 
+	{
+		String str;
+		Scanner in =new Scanner(System.in);
+		str=in.nextLine();
+		RevFunction(str);
+	}
+	public static void RevFunction(String str)
+	{
+		Node start=new Node('#');
+		int len=str.length();
+		for(int i=0;i<len;i++)
+		{
+			start=start.push(str.charAt(i));
+		}
+		String newstr="";
+		while(start.top()!='#')
+		{
+			newstr=newstr+start.top();
+			start=start.pop();
+		}
+		log("rev STring :"+newstr);
+	}
+	public static void log(String str)
+	{
+		System.out.println(str);
+	}
+	public static void PrintList(Node start)
+	{
+		start.printlist();
+	}
+	public static int StackSize(Node start)
+	{
+		return start.stacksize();
+	}
+}
